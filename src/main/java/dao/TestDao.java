6ca2496de3973cdb5b4512658aae8e763a7a1017
mina67gfan334 <tks2573474@stu.o-hara.ac.jp>
2026-05-11 11:37:23 +0900

    1回目修正版 TestDao

--- src/main/java/dao/TestDao.java
+++ src/main/java/dao/TestDao.java
@@ -1,64 +1,67 @@
 package dao;
 
+ 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+ 
+
 import bean.Student;
 import bean.Subject;
 import bean.Test;
 
-public class TestDao extends Dao {
+ 
 
-    // 成績検索
-    public List<Test> filter(int entYear, String classNum, String subjectCd) throws Exception {
+public class TestDao extends Dao {public List<Test> filter(int entYear, String classNum, String subjectCd) throws Exception {
 
-        List<Test> list = new ArrayList<>();
+    List<Test> list = new ArrayList<>();
 
-        try (Connection con = getConnection();
-             PreparedStatement st = con.prepareStatement(
+    try (Connection con = getConnection();
+         PreparedStatement st = con.prepareStatement(
 
-                 "SELECT * FROM TEST "
-               + "WHERE ENT_YEAR = ? "
-               + "AND CLASS_NUM = ? "
-               + "AND SUBJECT_CD = ?"
+            "SELECT * FROM TEST t "
+          + "JOIN STUDENT s ON t.STUDENT_NO = s.NO "
+          + "WHERE s.ENT_YEAR = ? "
+          + "AND s.CLASS_NUM = ? "
+          + "AND t.SUBJECT_CD = ?"
 
-             )) {
+         )) {
 
-            st.setInt(1, entYear);
-            st.setString(2, classNum);
-            st.setString(3, subjectCd);
+        st.setInt(1, entYear);
+        st.setString(2, classNum);
+        st.setString(3, subjectCd);
 
-            ResultSet rs = st.executeQuery();
+        ResultSet rs = st.executeQuery();
 
-            while (rs.next()) {
+        while (rs.next()) {
 
-                Test test = new Test();
+            Test test = new Test();
 
-                // 学生
-                Student student = new Student();
-                student.setNo(rs.getString("STUDENT_NO"));
-                student.setName(rs.getString("STUDENT_NAME"));
+            // ----------------------------
+            // Student
+            // ----------------------------
+            Student student = new Student();
+            student.setEntYear(rs.getInt("ENT_YEAR"));
+            student.setClassNum(rs.getString("CLASS_NUM"));
+            student.setNo(rs.getString("NO"));
+            student.setName(rs.getString("NAME"));
 
-                // 科目
-                Subject subject = new Subject();
-                subject.setCd(rs.getString("SUBJECT_CD"));
-                subject.setName(rs.getString("SUBJECT_NAME"));
+            test.setStudent(student);
 
-                test.setStudent(student);
-                test.setSubject(subject);
+            // ----------------------------
+            // 点数
+            // ----------------------------
+            test.setPoint(rs.getInt("POINT"));
 
-                // 点数
-                test.setPoint(rs.getInt("POINT"));
-
-                list.add(test);
-            }
-
-            rs.close();
+            list.add(test);
         }
 
-        return list;
+        rs.close();
     }
+
+    return list;
 }
