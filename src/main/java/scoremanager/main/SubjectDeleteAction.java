9a01d0c20cdd4de4a5b4ae5a2eee8298d268ebf5
nyadayo <tks2573446@stu.o-hara.ac.jp>
2026-05-15 09:58:55 +0900

    科目コードを変更

--- src/main/java/scoremanager/main/SubjectDeleteAction.java
+++ src/main/java/scoremanager/main/SubjectDeleteAction.java
@@ -11,25 +11,37 @@ import tool.Action;
 public class SubjectDeleteAction extends Action {
 
     @Override
+
     public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
         // セッション取得
+
         HttpSession session = req.getSession();
+
         Teacher teacher = (Teacher) session.getAttribute("user");
 
         // パラメータ取得（科目コード）
-        String cd = req.getParameter("cd");
+
+        String cd = req.getParameter("subject_cd");
 
         // DAO
+
         SubjectDao dao = new SubjectDao();
 
         // 科目取得（学校 + cd）
+
         Subject subject = dao.get(cd, teacher.getSchool());
 
         // リクエストにセット
+
         req.setAttribute("subject", subject);
 
         // 削除確認画面へ
+
         req.getRequestDispatcher("subject_delete.jsp").forward(req, res);
+
     }
+
 }
+
+ 
