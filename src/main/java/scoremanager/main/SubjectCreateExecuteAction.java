b159dbfea5168ae3077470a33f1cec6af0c5727c
nyadayo <tks2573446@stu.o-hara.ac.jp>
2026-05-15 09:56:25 +0900

    変数名の変更

--- src/main/java/scoremanager/main/SubjectCreateExecuteAction.java
+++ src/main/java/scoremanager/main/SubjectCreateExecuteAction.java
@@ -11,28 +11,44 @@ import tool.Action;
 public class SubjectCreateExecuteAction extends Action {
 
     @Override
+
     public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
         // ① セッション取得
+
         HttpSession session = req.getSession();
+
         Teacher teacher = (Teacher) session.getAttribute("user");
 
         // ② リクエストパラメータ取得
+
         String cd = req.getParameter("cd");      // 科目コード
+
         String name = req.getParameter("name");  // 科目名
 
         // ③ Subject オブジェクトにセット
+
         Subject subject = new Subject();
+
         subject.setCd(cd);
+
         subject.setName(name);
+
         subject.setSchool(teacher.getSchool());
 
         // ④ DB 登録
+
         SubjectDao subjectDao = new SubjectDao();
-        subjectDao.insert(subject);
+
+        subjectDao.save(subject);
 
         // ⑤ 登録完了画面へフォワード
+
         req.getRequestDispatcher("subject_create_done.jsp")
+
            .forward(req, res);
+
     }
+
 }
+ 
